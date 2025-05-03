296cad08e62161c8c4498e5f8b3a4b249c8a3a14
petargeorgiev18 <petar01920192@gmail.com>
2025-05-03 18:43:50 +0300

    Connection string changed

--- Orpheus/Program.cs
+++ Orpheus/Program.cs
@@ -11,7 +11,7 @@ namespace Orpheus
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("OrpheusDbConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<OrpheusDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
