fbb5a403602e7f7395c658bb7d3ec98923111c75
Your Name <your_email@example.com>
2025-05-03 17:00:36 +0300

    Add project files.

---
+++ Orpheus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Orpheus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
