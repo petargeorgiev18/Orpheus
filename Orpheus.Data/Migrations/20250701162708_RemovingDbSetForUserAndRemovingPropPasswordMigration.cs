456b5b8552c2606b810a3a3d6522107cb4181f7b
Your Name <your_email@example.com>
2025-07-01 19:35:17 +0300

    Added migration for the changes

---
+++ Orpheus.Data/Migrations/20250701162708_RemovingDbSetForUserAndRemovingPropPasswordMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orpheus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingDbSetForUserAndRemovingPropPasswordMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
